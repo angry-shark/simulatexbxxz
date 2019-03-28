78ccf74819235a42e4bffd86a743b2efb66d1542
argry-shark <73901217@qq.com>
2019-03-29 07:56:48 +0800

    test

--- src/pages/xianyuan.js
+++ src/pages/xianyuan.js
@@ -4,7 +4,15 @@ class xianyuan extends Component {
     render() {
         return (
             <div className="xianyuan">
-                鲜艳
+                <table>
+                    <tbody>
+                        <tr>
+                            <td><button>底肥山</button></td>
+                            <td><button>新造关</button></td>
+                            <td><button>南村城</button></td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         );
     }
